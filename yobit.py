b7616713406672c593942af0f0529639ae73553f
IIAIIITET <41283924+IIAIIITET@users.noreply.github.com>
2019-01-04 19:41:14 +0500

    Update yobit.py

--- yobit.py
+++ yobit.py
@@ -4,7 +4,7 @@ import requests
 
 def get_btc():
 	url 'https://yobit.net/api/2/btc_usd/ticker'
-	response = requests.get(url).content.json()
+	response = requests.get(url).json()
 	price = response['ticker']['last']
 	return str(price) + '$'
 
